5554a2dd052a19eb609819e95abb88b0cfeeeaa9
KiryakovIA <48943805+KiryakovIA@users.noreply.github.com>
2019-03-28 13:01:53 +0300

    Lesson1

--- JS/Script.js
+++ JS/Script.js
@@ -1,9 +1,9 @@
+console.log('Lesson 1')
 var tC = prompt('tC:');
 var tF = (9 / 5) * parseInt(tC) + 32;
-alert('tF: ' + tF);
-
+console.log('tF: ' + tF);
 var admin;
 var name = 'Василий';
 admin = name;
-alert(admin);
-alert(1000 + '108');
+console.log(admin);
+console.log(1000 + '108');
